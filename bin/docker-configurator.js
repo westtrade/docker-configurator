dc03d82c8709705820af93bcacf802fdc3ba3fec
Popov Gennadiy <me@westtrade.tk>
2016-12-01 07:42:25 +0300

    Some fixes in binary file

---
+++ bin/docker-configurator.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node --harmony
+
+/**
+* @Author: Popov Gennadiy <dio>
+* @Date:   2016-12-01T07:41:25+03:00
+* @Email:  me@westtrade.tk
+* @Last modified by:   dio
+* @Last modified time: 2016-12-01T07:41:51+03:00
+*/
+
+require('../index.js');
